859650bc93252f4ca12e6926fa0e0fe2bdfb346f
PhisitphongSamp <phisitphong.samp@bumail.net>
2023-05-05 04:57:06 +0700

    Add camera + gravity + Hp

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,14 +7,18 @@ public class InputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
-    private Vector2 movementInput;
+
     private PlayerMotor motor;
+    private PlayerLook look;
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+
         motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
         onFoot.Jump.performed += ctx => motor.Jump();
     }
 
@@ -22,6 +26,11 @@ public class InputManager : MonoBehaviour
     void FixedUpdate()
     {
         motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
