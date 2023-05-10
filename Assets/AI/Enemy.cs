9fde2a65b95bb835483873e96b234633c2553db5
ripper1277 <80735345+ripper1277@users.noreply.github.com>
2023-05-11 03:24:26 +0700

    Add Enemy Random Patrol + Enemy Melee Attack + Enemy Health + Enemy Spawn

--- Assets/AI/Enemy.cs
+++ Assets/AI/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,61 +6,102 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    Transform targetPlayer;
+    public string tagObject;
+    NavMeshAgent navMeshAgent;
+    public int maxHealth = 100;
+    private int currentHealth;
+    public int damage = 10;
+    public float attackRange = 2f;
+    public float detectionRange = 10f;
 
-    private StateMachine stateMachine;
-    private NavMeshAgent agent;
-    private GameObject player;
-    public NavMeshAgent Agent { get => agent; }
-    public GameObject Player { get => player;}
-    public Path path;
-    [Header("Sight Values")]
-    public float sightDistance = 20f;
-    public float fieldOfView = 85f;
-    public float eyeHeight;
-    [Header("Weapon Values")]
-    public Transform gunBarrel;
-    [Range(0.1f,10f)]
-    public float fireRate;
-    [SerializeField]
-    private string currentState;
-    // Start is called before the first frame update
-    void Start()
+     void Start()
     {
-        stateMachine = GetComponent<StateMachine>();
-        agent = GetComponent<NavMeshAgent>();
-        stateMachine.Initialise();
-        player = GameObject.FindWithTag("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        currentHealth = maxHealth;
+        if (targetPlayer == null)
+        {
+            targetPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 
-    // Update is called once per frame
+    internal void SetPatrolPoints(Transform[] transforms)
+    {
+        throw new NotImplementedException();
+    }
+
     void Update()
     {
-        CanSeePlayer();
-        currentState = stateMachine.activeState.ToString();
-    }
-    public bool CanSeePlayer()
-    {
-        if(player != null)
+        float distanceToPlayer = Vector3.Distance(transform.position, targetPlayer.position);
+
+        if (distanceToPlayer <= detectionRange)
         {
-            if(Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            if (distanceToPlayer <= attackRange)
             {
-                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
-                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
-                if (angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
-                {
-                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
-                    RaycastHit hitInfo = new RaycastHit();
-                    if(Physics.Raycast(ray, out hitInfo, sightDistance))
-                    {
-                        if(hitInfo.transform.gameObject == player)
-                        {
-                            return true;
-                        }
-                    }
-                    Debug.DrawRay(ray.origin, ray.direction * sightDistance);
-                }
+                AttackPlayer();
+            }
+            else
+            {
+                ChasePlayer();
             }
         }
-        return false;
+        
+    }
+
+    void AttackPlayer()
+    {
+        navMeshAgent.isStopped = true;
+        PlayerHealth playerHealth = targetPlayer.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+
+    void ChasePlayer()
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(targetPlayer.position);
+    }
+
+     public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(tagObject))
+        {
+            PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+            }
+
+            Destroy(other.gameObject);
+            navMeshAgent.isStopped = true;
+            StartCoroutine(ClearObject());
+        }
+    }
+
+    IEnumerator ClearObject()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 }
+
+
+
+
+
